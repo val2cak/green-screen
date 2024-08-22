ad20fa96f5c59169eff76b7be68bdf1e5d68a966
val2cak <valentina.tucak99@gmail.com>
2024-08-22 11:21:28 +0200

    feat: adding no data message to most watched

--- src/pages/most-watched/index.tsx
+++ src/pages/most-watched/index.tsx
@@ -8,6 +8,7 @@ import MovieCard from '@/components/movie-card/movie-card';
 import Filters from './components/filters';
 import { FiltersType } from '@/types/movie-types';
 import Loader from '@/components/loader/loader';
+import locale from '@/localization/locale';
 
 export const initialFilters = {
   year: '',
@@ -22,6 +23,8 @@ const MostWatchedPage = () => {
   const [filters, setFilters] = useState<FiltersType>(initialFilters);
 
   const router = useRouter();
+
+  const { noDataMessage } = locale.common;
 
   const fetchMovies = async (page: number, filters: FiltersType) => {
     const params: any = { page };
@@ -122,17 +125,21 @@ const MostWatchedPage = () => {
           resetMovies={resetMovies}
         />
 
-        <InfiniteScroll
-          dataLength={movies.length}
-          next={handleLoadMore}
-          hasMore={hasMore}
-          loader={<Loader />}
-          className='grid gap-y-4 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 grid-cols-5 2xl:grid-cols-6 !overflow-y-hidden'
-        >
-          {movies.map((movie, index) => (
-            <MovieCard key={`${movie.id}-${index}`} movie={movie} />
-          ))}
-        </InfiniteScroll>
+        {movies.length !== 0 ? (
+          <InfiniteScroll
+            dataLength={movies.length}
+            next={handleLoadMore}
+            hasMore={hasMore}
+            loader={<Loader />}
+            className='grid gap-y-4 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 grid-cols-5 2xl:grid-cols-6 !overflow-y-hidden'
+          >
+            {movies.map((movie, index) => (
+              <MovieCard key={`${movie.id}-${index}`} movie={movie} />
+            ))}
+          </InfiniteScroll>
+        ) : (
+          <div className='font-medium text-md'>{noDataMessage}</div>
+        )}
       </div>
     </Layout>
   );
